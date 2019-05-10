16356067a76c9d7ffec2b5e798bcd2ce505f5b07
MissLiDilayStruggle <18792313295@163.com>
2019-05-10 19:53:57 +0800

    李妍5-10提交

---
+++ strchr.c
@@ -0,0 +1,29 @@
+//#define _CRE_SECURE_NO_WARNNGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <assert.h>
+//
+//char *my_strchr(const char *str,const char ch)
+//{
+//	assert(str && ch);
+//	while(*str && *str != ch)
+//		str++;
+//	if(*str == ch)
+//		return str;
+//	else
+//		return NULL;
+//}
+//int main()
+//{
+//	char str[] = "hello";
+//	char ch = 'o';
+//	char *ret = my_strchr(str,ch);
+//
+//	if(ret ==NULL)
+//		printf("×Ö·û´®ÖÐÃ»ÓÐ²éÑ¯×Ö·û");
+//	else
+//		printf("×Ö·û´®ÖÐÓÐ²éÑ¯×Ö·û£º%s\n",ret);
+//
+//	system("pause");
+//	return 0;
+//}
