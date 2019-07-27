8497b960c7bfa0ee6e8f5bc4741a672f61f5bb52
MissLiDilayStruggle <18792313295@163.com>
2019-07-27 16:18:05 +0800

    几种常见的排序 第一次提交

---
+++ sort.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
+#include <assert.h>
+#include <stdbool.h>
+
+void InsertSort(int *a, int n);
+void ShellSort(int *a, int n);
+void SelectSort(int *a, int n);
+void HeapSort(int *a, int n);
+void BubbleSort(int *a, int n);
+void QuickSort1(int *a, int left, int right);
+void QuickSort2(int *a, int left, int right);
+void QuickSort3(int *a, int left, int right);
+void MergeSort(int *a, int n);
